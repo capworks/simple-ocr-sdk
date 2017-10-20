93a63e4e93a5d8c3d4881ad8075e4c9615facb6b
Jeanette Nielsen <jeanette@dinero.dk>
2017-10-20 13:32:43 +0200

    extracted interface for imagecontent

--- SimpleOcrSDK/OcrMetadata/Model/ImageContent.cs
+++ SimpleOcrSDK/OcrMetadata/Model/ImageContent.cs
@@ -4,21 +4,53 @@ using System.Linq;
 
 namespace OcrMetadata.Model
 {
-    public class ImageContent
+    public interface IImageContent
     {
         /// <summary>
         /// All sentences found on the image
         /// </summary>
-        public List<Sentence> Sentences { get; }
+        List<Sentence> Sentences { get; }
 
         /// <summary>
         /// All words found on the image.
         /// </summary>
-        public List<Word> Words { get;  }
+        List<Word> Words { get; }
 
         /// <summary>
         /// All lines found on the image. Dictionary with zero index line numbers as key, and list of sentences as value.
         /// </summary>
+        Dictionary<int, List<Sentence>> Lines { get; set; }
+
+        /// <summary>
+        /// Get the content as a plain text string on one line.
+        /// </summary>
+        /// <param name="sentenceSeperator">Pr. default the text sentences is seperated by a space. You can define your own seperator, fx linebreak "\n" or colon ";"</param>
+        /// <returns></returns>
+        string GetPlainText(string sentenceSeperator = " ");
+
+        /// <summary>
+        /// Get the content as a plain text string, but each line is on a new line.
+        /// </summary>
+        /// <param name="sentenceSeperator">Pr. default the text sentences is seperated by a space. You can define your own seperator, fx linebreak "\n" or colon ";"</param>
+        /// <returns></returns>
+        string GetPlainTextWithLineBreaks(string sentenceSeperator = " ");
+
+        /// <summary>
+        /// Is any text found on the image
+        /// </summary>
+        /// <returns>Returns true if any text was found.</returns>
+        bool TextFound();
+    }
+
+    public class ImageContent : IImageContent
+    {
+        /// <inheritdoc />
+        public List<Sentence> Sentences { get; }
+
+        /// <inheritdoc />
+        public List<Word> Words { get;  }
+
+        /// <inheritdoc />
         public Dictionary<int, List<Sentence>> Lines { get; set; }
 
         public ImageContent(List<Sentence> sentences)
@@ -28,21 +60,13 @@ namespace OcrMetadata.Model
             Lines = Sentences.GroupBy(x => x.Line).ToDictionary(g => g.Key, g => g.ToList());
         }
 
-        /// <summary>
-        /// Get the content as a plain text string on one line.
-        /// </summary>
-        /// <param name="sentenceSeperator">Pr. default the text sentences is seperated by a space. You can define your own seperator, fx linebreak "\n" or colon ";"</param>
-        /// <returns></returns>
+        /// <inheritdoc />
         public string GetPlainText(string sentenceSeperator = " ")
         {
             return GetAggregatedValue(Sentences, sentenceSeperator);
         }
 
-        /// <summary>
-        /// Get the content as a plain text string, but each line is on a new line.
-        /// </summary>
-        /// <param name="sentenceSeperator">Pr. default the text sentences is seperated by a space. You can define your own seperator, fx linebreak "\n" or colon ";"</param>
-        /// <returns></returns>
+        /// <inheritdoc />
         public string GetPlainTextWithLineBreaks(string sentenceSeperator = " ")
         {
             return Lines.Aggregate("", (current, wd) => current + (string.IsNullOrEmpty(current)? "" : "\n") + GetAggregatedValue(wd.Value, sentenceSeperator));
@@ -53,10 +77,7 @@ namespace OcrMetadata.Model
             return sentences.Aggregate("", (current, wd) => current + (string.IsNullOrEmpty(current) ? "" : sentenceSeperator) + wd.Value);
         }
 
-        /// <summary>
-        /// Is any text found on the image
-        /// </summary>
-        /// <returns>Returns true if any text was found.</returns>
+        /// <inheritdoc />
         public bool TextFound()
         {
             return Sentences.Any();
